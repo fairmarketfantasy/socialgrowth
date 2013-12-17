153e189a80958bf352145a2b5f470e8a3f680d91
Dosty Everts <dostman@luckynerds.com>
2013-12-17 10:31:28 -0600

    Created initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialgrowth::Application.config.secret_key_base = 'a1d3420b3f885b50657ae83fec91fe008e3c0bd4a45eb6312cf63a1a9d48fb5c5a17dbd7c9bcff27c9beb6dcc202fadb6a85287f6eb7d3951916e790c64c1dbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialgrowth::Application.config.session_store :cookie_store, key: '_socialgrowth_session'
