78bad858c48da4b7680f58a090fce2e0149fe0bf
Mike Ihbe <mihbe@yammer-inc.com>
2014-01-06 16:01:29 -0800

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131231214924) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
     t.string   "provider"
@@ -59,6 +62,20 @@ ActiveRecord::Schema.define(version: 20131231214924) do
     t.datetime "updated_at"
   end
 
+  create_table "excluded_terms", force: true do |t|
+    t.integer  "search_term_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "search_terms", force: true do |t|
+    t.integer  "campaign_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -75,7 +92,7 @@ ActiveRecord::Schema.define(version: 20131231214924) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
