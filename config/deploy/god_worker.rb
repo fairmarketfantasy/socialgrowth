bcc3f6b15ad3ee14ed3483c72eab62c7f958b8b9
Dosty Everts <dostman@luckynerds.com>
2014-01-22 19:18:51 -0600

    Updated worker file to run proper rake task, and changed names over

--- config/deploy/god_worker.rb
+++ config/deploy/god_worker.rb
@@ -1,16 +1,17 @@
-=begin
+begin
 APP_NAME = 'socialgrowth'
 BASE_DIR = "/mnt/www/#{APP_NAME}"
 PID_PATH = "#{BASE_DIR}/shared/pids"
 God.pid_file_directory = PID_PATH
 
 begin
+
 God.watch do |w|
-  pid_file = PID_PATH + "/markettender.pid"
-  w.name = "markettender"
-  w.start = "bundle exec rake start_all_campaigns --trace"
+  pid_file = PID_PATH + "/socialgrowth.pid"
+  w.name = "socialgrowth"
+  w.start = "bundle exec rake campaign:start --trace"
   w.dir = BASE_DIR + '/current/webapp'
-  w.log = BASE_DIR + '/shared/log/markettender.log'
+  w.log = BASE_DIR + '/shared/log/socialgrowth.log'
   w.env = {"RAILS_ENV" => ENV['RAILS_ENV'],
            "PIDFILE" => pid_file}
   w.stop          = -> { `kill -s KILL #{IO.read(pid_file)}` }
@@ -18,4 +19,3 @@ God.watch do |w|
   w.restart_grace = 5.seconds
   w.keepalive#(:memory_max => 150.megabytes, :cpu_max => 50.percent)
 end
-=end
